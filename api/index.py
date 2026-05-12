4bb72fae895841bd2bd65fd99d6948660c0e0d32
Ricardomonrreal <230300797@ucaribe.edu.mx>
2026-05-11 21:55:08 -0500

    Fix tree map 1.1

--- api/index.py
+++ api/index.py
@@ -293,7 +293,7 @@ def analytics_categories():
         SELECT t.product_category_name_english, SUM(oi.price) as revenue, COUNT(oi.product_id) as sales
         FROM olist_order_items oi
         JOIN olist_products p ON oi.product_id = p.product_id
-        JOIN product_category_name_translation t ON p.product_category_name = t.product_category_name
+        JOIN product_category_translation t ON p.product_category_name = t.product_category_name
         GROUP BY t.product_category_name_english
         ORDER BY revenue DESC
         LIMIT 10
@@ -305,7 +305,7 @@ def analytics_categories():
             SELECT t.product_category_name_english
             FROM olist_order_items oi
             JOIN olist_products p ON oi.product_id = p.product_id
-            JOIN product_category_name_translation t ON p.product_category_name = t.product_category_name
+            JOIN product_category_translation t ON p.product_category_name = t.product_category_name
             GROUP BY t.product_category_name_english
             ORDER BY SUM(oi.price) DESC
             LIMIT 10
@@ -313,7 +313,7 @@ def analytics_categories():
         SELECT p.product_id, t.product_category_name_english, COUNT(oi.product_id) as sales, SUM(oi.price) as revenue
         FROM olist_order_items oi
         JOIN olist_products p ON oi.product_id = p.product_id
-        JOIN product_category_name_translation t ON p.product_category_name = t.product_category_name
+        JOIN product_category_translation t ON p.product_category_name = t.product_category_name
         JOIN TopCat tc ON tc.product_category_name_english = t.product_category_name_english
         GROUP BY p.product_id, t.product_category_name_english
         ORDER BY revenue DESC
